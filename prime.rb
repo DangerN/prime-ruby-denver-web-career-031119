9af40491dce56a774994b409d729645f8c2f964f
convivial-system-4430 <cdneal091@gmail.com>
2019-03-02 23:55:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 # Add  code here!
 def prime?(number)
   num = number.abs
-  if num == 1
-    return false
-  end
   for d in 2..(num-1)
   if (num % d) == 0
     return false
   end
+  end
+  if num == 0 || 1
+    return false
   end
   true
 end
