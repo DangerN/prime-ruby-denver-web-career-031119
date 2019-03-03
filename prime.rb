403989e5686c2c60c45b163ef94ccdbada25e5c6
convivial-system-4430 <cdneal091@gmail.com>
2019-03-03 00:36:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(number)
   if num == 1 || 0
     return false
   end
-  check = (2...num).to_a
+  check = (2..num).to_a
   check.each do |it|
     if ((num % it) == 0) && (num != it)
       return false
