188676f3dac70327560736771ea24d2edd24eee3
convivial-system-4430 <cdneal091@gmail.com>
2019-03-03 00:36:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,15 @@
 # Add  code here!
 def prime?(number)
   num = number.abs
+  if num == 1 || 0
+    return false
+  end
   check = (2...num).to_a
   check.each do |it|
     if ((num % it) == 0) && (num != it)
       return false
     end
   end
-  if num == 1 || 0
-    return false
-  end
+
   true
 end
